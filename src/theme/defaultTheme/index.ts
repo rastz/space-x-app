3325acbb156d878311c49c5a3dddd617041c2ed9
Mateusz Podgórski <mateusz.podgorski@home.net.pl>
2020-05-18 06:42:31 +0200

    Create default theme for aplication

---
+++ src/theme/defaultTheme/index.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+	fontFamily: {
+		name: "Roboto, sans-serif",
+		href: "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap",
+	},
+};
+
+export default theme;
