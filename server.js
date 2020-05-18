0b0c8052c37cb7d39a38b848c881501e53806d90
Mateusz Podgórski <mateusz.podgorski@home.net.pl>
2020-05-18 06:39:43 +0200

    Add SERVER_PORT env variable

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(
 	})
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.SERVER_PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`Server started on port: ${PORT}`);
